dd4cd843cd61bfc0095de19339d46822d4342329
JackyWongCW <hello@jackywongcw.com>
2021-03-28 14:45:22 +0800

    Added mapkit to project
    Mapkit by default would center to Singapore's coordinates

--- Trafficker/ViewController.swift
+++ Trafficker/ViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var mapView: MKMapView!
+    
+    private let sgLat = 1.3521
+    private let sgLong = 103.8198
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let sgInitialLocation = CLLocation(latitude: sgLat,
+                                           longitude: sgLong)
+        
+        centerMapOnLocation(sgInitialLocation)
+        
+        
     }
-
-
+    
+    // Map func
+    func centerMapOnLocation(_ location: CLLocation) {
+        
+        // Size of Singapore ~ 728 * 1000
+        let regionRadius: CLLocationDistance = 1000
+        let coordinateRegion = MKCoordinateRegion(center: location.coordinate,
+                                                  latitudinalMeters: regionRadius * 50.0,
+                                                  longitudinalMeters: regionRadius * 50.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
 }
 
